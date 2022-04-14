c5ac15c7ef8ed06da66f789cbbbc486aa420e500
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-14 16:42:04 +0300

    Messages added

---
+++ src/main/java/com/emrekaraman/enocaproject/core/constants/Messages.java
@@ -0,0 +1,18 @@
+package com.emrekaraman.enocaproject.core.constants;
+
+public class Messages {
+
+    public static final String USER_FOUND = "User found";
+    public static final String COMPANY_FOUND = "Company found";
+    public static final String SUCCESS_ADDED = "Success added";
+    public static final String SUCCESS_DELETED = "Success deleted";
+    public static final String SUCCESS_UPDATED = "Success updated";
+    public static final String SUCCESS_GET = "Success get";
+
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String COMPANY_NOT_FOUND = "Company not found";
+    public static final String FAILED_ADDED = "Failed added";
+    public static final String FAILED_DELETED = "Failed deleted";
+    public static final String FAILED_UPDATED = "Failed updated";
+    public static final String FAILED_GET = "Failed get";
+}
