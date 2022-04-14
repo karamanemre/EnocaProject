389db7ac2d5f03d2429f4cb577fc10898e792e8d
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-14 16:41:25 +0300

    result structure established

---
+++ src/main/java/com/emrekaraman/enocaproject/core/utilities/SuccessResult.java
@@ -0,0 +1,12 @@
+package com.emrekaraman.enocaproject.core.utilities;
+
+public class SuccessResult extends Result {
+
+    public SuccessResult(){
+        super(true);
+    }
+
+    public SuccessResult(String message){
+        super(true,message);
+    }
+}
